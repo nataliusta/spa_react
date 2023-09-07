5a3bf47ac212de6bdd7c76bfe22c96052dd0fd96
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-09-07 15:59:38 +0300

    Add Link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const HomePage = () => {
     return (
-        <h1>My Home Page</h1>
+        <>
+            <h1>My Home Page</h1>
+            <p>
+                Go to <Link to='/products'>the list of products</Link>
+            </p>
+        </>
     )
 };
 
