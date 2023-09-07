45b7bd6ba5e007b3ba84e8e7a4b51499d8c77413
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-09-07 21:35:54 +0300

    Add error page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
 import Products from './pages/Products';
 import Root from './pages/Root';
+import Error from './pages/Error';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <Error />,
     children: [
       {path: '/', element: <HomePage /> },
       {path: '/products', element: <Products /> },
--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,12 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import MainNavigation from '../components/MainNavigation';
-import styles from './Root.module.css';
 
 const Root = () => {
     return (
         <>
             <h1><MainNavigation /></h1>
-            <main className={styles.content}>
+            <main>
                 <Outlet />
             </main>
         </>
