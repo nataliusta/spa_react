1783918237e24f5e7f635ce170872cf253b60c54
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-09-08 13:27:26 +0300

    Update Products.js

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,21 @@
+import { Link } from 'react-router-dom';
+
+const PROUCTS = [
+    {id: 'p1', title: 'Product 1'},
+    {id: 'p2', title: 'Product 2'},
+    {id: 'p3', title: 'Product 3'},
+];
+
 const Products = () => {
     return (
         <>
             <h1>The Products Page</h1>
             <ul>
-                <li>Product 1</li>
-                <li>Product 2</li>
-                <li>Product 3</li>
+                {PROUCTS.map((prod) => (
+                    <li key={prod.id}>
+                        <Link to={prod.id} relative=''>{prod.title}</Link>
+                    </li>
+                ))}
             </ul>
         </>
     );
